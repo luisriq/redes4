03cdb97803b26d0436178e15acc89c9998e38cc5
luis <luis.9993@gmail.com>
2016-06-15 13:07:01 -0400

    init

---
+++ laboratorio4.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from scipy.io.wavfile import read, write
+import matplotlib.pyplot as plt
+import matplotlib
+import math
+from scipy.fftpack import fft, fftfreq, ifft
+from scipy.signal import *
+
+
+input("Presione enter para salir:\n")
